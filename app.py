1d23d9db9a5a7c2b51e69bac42342d476dbcb137
Caio Araújo <caio.araujo@PPM-SPO-02577.local>
2020-09-06 20:37:13 -0300

    new features, too lazy to explain

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, argparse, functools, datetime , string , operator
+import os, argparse, functools, datetime , string , operator , json , re
 
 
 def parse_file(file) :
@@ -10,10 +10,10 @@ def parse_file(file) :
 		return {"title": "","date": ""}
 
 
-def make_index( root, dirs, files ) :
+def make_index( root, dirs, files , cfg ) :
 	path = os.path.abspath(root)
 
-	template = open("templates/index.html","r").read()
+	template = open(cfg["theme"],"r").read()
 
 	table_html = ""
 
@@ -40,7 +40,7 @@ def make_index( root, dirs, files ) :
 		metadata = parse_file( os.path.join( path , f ) )
 		files_dated.append([f,metadata["title"],metadata["date"]])		
 
-	files_dated = sorted( files_dated, key=operator.itemgetter(2) )
+	files_dated = sorted( files_dated, key=operator.itemgetter(2) , reverse=True)
 
 	for f in files_dated :
 		table_html += """<tr>
@@ -48,11 +48,20 @@ def make_index( root, dirs, files ) :
 	<td>%s</td>
 	<td>%s</td>
 </tr>
-""" %	( f[0] ,f[0], f[1] ,( datetime.datetime.fromtimestamp(int(f[2])).strftime("%d/%m/%Y") if f[2].isdigit() else f[2] ) )
+""" % ( f[0] ,f[0], f[1] ,( datetime.datetime.fromtimestamp(int(f[2])).strftime("%d/%m/%Y") if f[2].isdigit() else f[2] ) )
 
-	html_result = string.Template( template ).substitute( { "posts_table" : table_html , "blog_title": "lude.rs"} )
+	if len(files_dated) > 0 :
+	    latest = "<h3>Latest Entry:</h3><hr>"
+	    latest += open(os.path.join(path,files_dated[0][0]), encoding="utf-8" ).read()
+	    if files_dated[0][0][-3:] == "txt" :
+	      latest = latest.replace("\n","<br>")
+	else :
+	    latest = ""
 
-	index_file = open(os.path.join( path , "index.html" ), "w")
+	html_result = string.Template( template ).substitute( { "posts_table" : table_html ,
+	"blog_title": cfg["title"] , "latest_entry":latest} )
+
+	index_file = open(os.path.join( path , "index.html" ) , "w" ,encoding="utf-8" )
 	index_file.write(html_result)
 	index_file.close()
 
@@ -60,18 +69,20 @@ def main(args) :
 	
 	path = os.path.abspath(args.path)
 
+	config = json.loads(open(args.config).read())
+
 	for root, dirs, files in os.walk(path) :
-		files = [f for f in files if not f[0] == '.']
+		files = [f for f in files if not f[0] == '.' and f not in config["ignore"] ]
 		dirs[:] = [d for d in dirs if not d[0] == '.'] # ignore hidden files/dirs
 
-		make_index( os.path.join(path,root) , dirs , files )
+		make_index( os.path.join(path,root) , dirs , files , config )
 
 	
 
 if __name__ == "__main__" :
 	parser = argparse.ArgumentParser(description="Create a blog from .txt files")
 	parser.add_argument('-p','--path',help="Path of the files.")
-	
+	parser.add_argument('-c','--config',help='Config file.',default="default_config.json")	
 	args = parser.parse_args()
 
 	main(args)
